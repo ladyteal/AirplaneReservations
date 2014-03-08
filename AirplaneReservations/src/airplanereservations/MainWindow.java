f1aa937cc4d96e8da87da8ae9b92cea65348fe34
rustadmd <markrustad05@gmail.com>
2014-03-08 12:44:52 -0600

    Began setting out your information panel

--- AirplaneReservations/src/airplanereservations/MainWindow.java
+++ AirplaneReservations/src/airplanereservations/MainWindow.java
@@ -8,19 +8,26 @@ package airplanereservations;
 
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.border.Border;
 /**
  *
- * @author Mark
+ * @author Mark Rustad
  */
 public class MainWindow extends JFrame{
 
-    /**
-     * @param args the command line arguments
-     */
-    //private JFrame main;
-    private JPanel mainPanel, //Main page to work with
+    //various panels
+    private JPanel flightInformationPanel, //Contains the flight information
+            yourInformationPanel, //displays your information
+            legendPanel, //gives user definitions for symbols
             seatGrid; //Grid for the seats
+    
+    //changing display information
+    JTextField custName_TF, custEmail_TF;//input fields for email and name
+    JLabel selectedSeatsDisplay; // Label to display the selected seats
+     
+    //seat-related arrays
     private JButton [] buttonArray;//array to store the buttons
+    private String[] seatNames;
     
     public MainWindow()
     {
@@ -34,9 +41,9 @@ public class MainWindow extends JFrame{
         
         
         setLayout(new BorderLayout());
-        getContentPane().add(new JLabel ("Testing"), BorderLayout.EAST);
         
         createSeats();
+        createYourInformationPanel();
         pack();
         setVisible(true);
     }
@@ -46,6 +53,8 @@ public class MainWindow extends JFrame{
         // set the number of rows
         int numRows = 10;
         int numSeatsInRow = 4;
+        //get the total seats
+        int totalSeats = numRows * numSeatsInRow;
         
         //set the grid layout
         seatGrid = new JPanel();
@@ -54,7 +63,8 @@ public class MainWindow extends JFrame{
         seatGrid.setVisible(true);
         
         int seatIndex =0;//keep track of the number of seats
-        buttonArray = new JButton[numRows * numSeatsInRow];
+        buttonArray = new JButton[totalSeats];
+        seatNames = new String[totalSeats];
        
         //loop for each row
         for (int rowCount = 0; rowCount < numRows; rowCount++)
@@ -63,17 +73,37 @@ public class MainWindow extends JFrame{
             //for each seat in the row
             for (int seatCount = 0; seatCount <= numSeatsInRow; seatCount++)
             {
+                int aislePosition = numSeatsInRow/2;
                 //middle of the seats should be an aisle
-                if (seatCount != numSeatsInRow/2)
+                if (seatCount != aislePosition)
                 {
+                    //declare the letter the seat will be
+                    char seatID;
+                    int firstSeatIDAscii = 65;
+                    //before aisle, simple addition
+                    if(seatCount < aislePosition)
+                    {
+                        seatID = (char)(firstSeatIDAscii + seatCount);
+                    }
+                    //after aisle, take 1 away
+                    else
+                    {
+                        seatID = (char)(firstSeatIDAscii + seatCount-1);
+                    }
+                    
+                    //declare the seat's name
+                    int rowName = rowCount + 1;
+                    String seatName = "Seat " + rowName + seatID;
+                    // add the seatname to a table
                     //create a new button
-                JButton newButton = new JButton("Seat " + seatCount);
-                //add it to the buttonArray
-                buttonArray[seatIndex] = newButton;
-                //increment seat index
-                seatIndex++;
-                //add it to the grid
-                seatGrid.add(newButton);
+                    JButton newButton = new JButton(seatName);
+                    //add it to the buttonArray
+                    buttonArray[seatIndex] = newButton;
+                    seatNames[seatIndex] = seatName;
+                    //increment seat index
+                    seatIndex++;
+                    //add it to the grid
+                    seatGrid.add(newButton);
                 }
                 else
                 {
@@ -85,6 +115,34 @@ public class MainWindow extends JFrame{
         }
         
         
+    }
+    
+    public void createYourInformationPanel()
+    {
+        yourInformationPanel = new JPanel();
+        yourInformationPanel.setLayout(new BorderLayout());
+        add(yourInformationPanel, BorderLayout.EAST);
+        yourInformationPanel.setVisible(true);
+        //add title to the box
+        Border border = BorderFactory.createTitledBorder("Your Information");
+        yourInformationPanel.setBorder(border);
+        //add labels to the West side
+        JPanel labelPanel = new JPanel();
+        labelPanel.setLayout(new FlowLayout());
+        JLabel custNameLabel = new JLabel("Name:");
+       yourInformationPanel.add(custNameLabel, BorderLayout.WEST);
+       
+       JLabel custEmailLabel = new JLabel("Email:");
+       yourInformationPanel.add(custEmailLabel, BorderLayout.WEST);
+       
+       JLabel selectedSeatsLabel = new JLabel("Selected Seats:");
+       yourInformationPanel.add(selectedSeatsLabel, BorderLayout.WEST);
+       
+        //add display in the CENTER
+       custName_TF = new JTextField();
+      // yourInformationPanel.add(custName_TF, BorderLayout.CENTER);
+        //add panel to south
+        //add purchase button to south
     }
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
