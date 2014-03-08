6de658f4adb34984fbb2d1d7bbf1c4b66876a687
rustadmd <markrustad05@gmail.com>
2014-03-08 15:46:13 -0600

    Completed adding components to "Your information" and "Flight Information" panes

--- AirplaneReservations/src/airplanereservations/MainWindow.java
+++ AirplaneReservations/src/airplanereservations/MainWindow.java
@@ -16,15 +16,19 @@ import javax.swing.border.Border;
 public class MainWindow extends JFrame{
 
     //various panels
-    private JPanel flightInformationPanel, //Contains the flight information
+    private JPanel 
+            infoPanel, //holds the information panels
+            flightInformationPanel, //Contains the flight information
             yourInformationPanel, //displays your information
             legendPanel, //gives user definitions for symbols
             seatGrid; //Grid for the seats
     
     //changing display information
-    JTextField custName_TF, custEmail_TF;//input fields for email and name
-    JLabel selectedSeatsDisplay; // Label to display the selected seats
-     
+    private JTextField custName_TF, custEmail_TF;//input fields for email and name
+    private JLabel selectedSeatsDisplay, totalPriceDisplay; // Label to display the selected seats
+    private JButton purchaseButton; 
+    
+    
     //seat-related arrays
     private JButton [] buttonArray;//array to store the buttons
     private String[] seatNames;
@@ -42,7 +46,15 @@ public class MainWindow extends JFrame{
         
         setLayout(new BorderLayout());
         
+        //ready the info panel 
+        infoPanel = new JPanel();
+        int numInfoPanels = 2;//your/flight information
+        infoPanel.setLayout(new GridLayout(numInfoPanels, 1));
+        add(infoPanel, BorderLayout.EAST);
+        
+        //add the individual panels
         createSeats();
+        createFlightInformation();
         createYourInformationPanel();
         pack();
         setVisible(true);
@@ -121,28 +133,104 @@ public class MainWindow extends JFrame{
     {
         yourInformationPanel = new JPanel();
         yourInformationPanel.setLayout(new BorderLayout());
-        add(yourInformationPanel, BorderLayout.EAST);
+        infoPanel.add(yourInformationPanel, BorderLayout.EAST);
         yourInformationPanel.setVisible(true);
         //add title to the box
         Border border = BorderFactory.createTitledBorder("Your Information");
         yourInformationPanel.setBorder(border);
-        //add labels to the West side
+        
+        //set up the label panel
+        int numLabels = 4;
         JPanel labelPanel = new JPanel();
-        labelPanel.setLayout(new FlowLayout());
+        labelPanel.setLayout(new GridLayout(numLabels, 2));
+        yourInformationPanel.add(labelPanel, BorderLayout.WEST);
+        
+        //add name fields to the label panel
         JLabel custNameLabel = new JLabel("Name:");
-       yourInformationPanel.add(custNameLabel, BorderLayout.WEST);
-       
-       JLabel custEmailLabel = new JLabel("Email:");
-       yourInformationPanel.add(custEmailLabel, BorderLayout.WEST);
-       
-       JLabel selectedSeatsLabel = new JLabel("Selected Seats:");
-       yourInformationPanel.add(selectedSeatsLabel, BorderLayout.WEST);
-       
-        //add display in the CENTER
+        labelPanel.add(custNameLabel);
        custName_TF = new JTextField();
-      // yourInformationPanel.add(custName_TF, BorderLayout.CENTER);
+       labelPanel.add(custName_TF);
+       
+       //add the email fields
+       JLabel custEmailLabel = new JLabel("Email:");
+       labelPanel.add(custEmailLabel);
+       custEmail_TF = new JTextField();
+       labelPanel.add(custEmail_TF);
+       
+       //add the selected seats fields
+       JLabel selectedSeatsLabel = new JLabel("Selected Seats:");
+       labelPanel.add(selectedSeatsLabel);
+       selectedSeatsDisplay = new JLabel("Test Text for seats");
+       labelPanel.add(selectedSeatsDisplay);
+       
+        //add the money fields
+       JLabel totalPriceLabel = new JLabel("Total Price:");
+       labelPanel.add(totalPriceLabel);
+       totalPriceDisplay = new JLabel("Test Price");
+       labelPanel.add(totalPriceDisplay);
+       
         //add panel to south
+       JPanel purchaseButtonPanel = new JPanel();
+       yourInformationPanel.add(purchaseButtonPanel, BorderLayout.SOUTH);
+       purchaseButtonPanel.setLayout(new FlowLayout());
+       
         //add purchase button to south
+       purchaseButton = new JButton("Purchase Seats");
+       purchaseButtonPanel.add(purchaseButton);
+    }
+    
+    public void createFlightInformation()
+    {
+        //All of the flight information
+        
+        String airlineName = "SpeedyJet Airways";
+        String departureCity = "MSP";
+        String departureTime = "1:30 PM, 6/12/2014";
+        String arrivalCity = "LAX";
+        String arrivalTime = "5:40 PM, 6/12/2014";
+        String aircraftName = "Boeing 737";
+        String flightNumber_S = "#3667";
+        
+        //settings for the flight panel
+        flightInformationPanel = new JPanel();
+        infoPanel.add(flightInformationPanel, BorderLayout.EAST);
+        //add a title to the panel
+        Border border = BorderFactory.createTitledBorder("Flight Information");
+        flightInformationPanel.setBorder(border);
+        
+        //set up the layout of the panel
+        int numDisplayFields = 5;
+        flightInformationPanel.setLayout(new GridLayout(numDisplayFields, 2));
+        
+        //add the airline information/label
+        JLabel airlineLabel = new JLabel("Airline:");
+        flightInformationPanel.add(airlineLabel);
+        JLabel airlineInfo = new JLabel(airlineName);
+        flightInformationPanel.add(airlineInfo);
+        
+        //add departure label/info
+        JLabel departLabel = new JLabel("Departure Information:");
+        flightInformationPanel.add(departLabel);
+        JLabel deptDisplay = new JLabel(departureCity + " " + departureTime);
+        flightInformationPanel.add(deptDisplay);
+        
+        //add arrival label/info
+        JLabel arrivalLabel = new JLabel("Arrival Information:");
+        flightInformationPanel.add(arrivalLabel);
+        JLabel arrivalInfo = new JLabel(arrivalCity + " " + arrivalTime);
+        flightInformationPanel.add(arrivalInfo);
+        
+        //add aircraft info
+        JLabel aircraftLabel = new JLabel("Aircraft:");
+        flightInformationPanel.add(aircraftLabel);
+        JLabel aircraftInfo = new JLabel(aircraftName);
+        flightInformationPanel.add(aircraftInfo);
+        
+        //add flight number
+        JLabel flightLabel = new JLabel("Flight Number:");
+        flightInformationPanel.add(flightLabel);
+        JLabel flightInfo = new JLabel(flightNumber_S);
+        flightInformationPanel.add(flightInfo);
     }
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
